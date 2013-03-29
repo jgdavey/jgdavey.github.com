a28f97b151d7c01aea688da76d1bebc5334e5ed3
Joshua Davey <josh@joshuadavey.com>
2013-03-28 23:32:22 -0500

    Site updated at 2013-03-29 04:32:22 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -10,19 +10,22 @@ var github = (function(){
   return {
     showRepos: function(options){
       $.ajax({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
-        , type: 'jsonp'
+          url: "https://api.github.com/users/"+options.user+"/repos"
+        , dataType: 'json'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
+          if (!data) { return; }
+          for (var i = 0; i < data.length; i++) {
+            var ignore = options.ignore && $.inArray(data[i].name, options.ignore) != -1;
+            if ((options.skip_forks && data[i].fork) || ignore) {
+              continue;
+            }
+            repos.push(data[i]);
           }
           repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
+            var aDate = new Date(a.updated_at).valueOf(),
+                bDate = new Date(b.updated_at).valueOf();
 
             if (aDate === bDate) { return 0; }
             return aDate > bDate ? -1 : 1;
