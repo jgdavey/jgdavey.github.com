43467a2b29aed40ba822144530e4cd117ed77a6a
Joshua Davey <josh@joshuadavey.com>
2014-05-02 09:42:20 -0500

    Site updated at 2014-05-02 14:42:20 UTC

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,1 @@
-(function(doc) {
-  var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-  function fix() {
-    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-    doc.removeEventListener(type, fix, true);
-  }
-  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-    fix();
-    scales = [0.25, 1.6];
-    doc[addEvent](type, fix, true);
-  }
-}(document));
+!function(e){function t(){a.content="width=device-width,minimum-scale="+r[0]+",maximum-scale="+r[1],e.removeEventListener(i,t,!0)}var n="addEventListener",i="gesturestart",m="querySelectorAll",r=[1,1],a=m in e?e[m]("meta[name=viewport]"):[];(a=a[a.length-1])&&n in e&&(t(),r=[.25,1.6],e[n](i,t,!0))}(document);
